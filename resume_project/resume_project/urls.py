ef2ac01f425037fd9cb9545f8e596af834bd77b6
root <artem.artem64@yandex.ru>
2019-04-21 12:25:21 +0300

    configured static

--- resume_project/resume_project/urls.py
+++ resume_project/resume_project/urls.py
@@ -13,13 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
+import django.views.static
 
-from resume import views
+
+from resume_project import settings
+import django.views.static
+
+from resume import views, auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("login/", auth_views.login_page),
     path('', views.main)
 ]
+
+if settings.DEBUG == False:
+    urlpatterns += url(r'^static/(?P<path>.*)$', django.views.static.serve, {'document_root': settings.STATIC_URL, 'show_indexes': settings.DEBUG}),
