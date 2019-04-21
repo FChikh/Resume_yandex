7dc13e462175994e80ecc7d1a809ad2a42e6e491
root <artem.artem64@yandex.ru>
2019-04-21 12:29:00 +0300

    added login_required for main page

--- resume_project/resume/views.py
+++ resume_project/resume/views.py
@@ -1,7 +1,14 @@
+from random import randint
+
+from django.contrib import messages
+from django.contrib.auth import login, logout, update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 from django.shortcuts import render, redirect
 
 
-
+@login_required
 def main(request):  # main page
     data = dict()
     return render(request, "main.html", data)
