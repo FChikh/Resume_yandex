0ed2663cade72aa9121204e97665be05fc6a46a9
root <artem.artem64@yandex.ru>
2019-04-21 11:12:02 +0300

    allowed local host

--- ResumeYandex/ResumeYandex/settings.py
+++ ResumeYandex/ResumeYandex/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@txh*nb!6k--02!wc8g*@@@--htxc_#og+wbqv0^4ga^b&+8=&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
