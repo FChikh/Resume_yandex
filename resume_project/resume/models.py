80c07970ce66807149123a79f02cdb00c81244ae
Fedor Chikhachev <kal3vala@MacBook-Pro-Fedor.local>
2019-04-27 16:07:40 +0300

    github login form added

--- resume_project/resume/models.py
+++ resume_project/resume/models.py
@@ -1,15 +1,17 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # Create your models here.
 
-class Github_connected_users(models.Model):
-  id = models.AutoField(primary_key=True)
-  authorid = models.IntegerField()
-  fullname = models.CharField(max_length=144)
-  avatarurl = models.CharField(max_length=244)
-  orgs = models.CharField(max_length=90000)
-  followers = models.IntegerField()
-  repos_dict_with_full_info = models.CharField(max_length=90000)
-  client_ID_and_secret = models.CharField(max_length=144)
+class GithubConnectedUsers(models.Model):
+    id = models.AutoField(primary_key=True)
+    authorid = models.IntegerField()
+    fullname = models.CharField(max_length=144)
+    avatarurl = models.CharField(max_length=244)
+    orgs = models.CharField(max_length=90000)
+    followers = models.IntegerField()
+    repos_dict_with_full_info = models.CharField(max_length=90000)
+    client_ID_and_secret = models.CharField(max_length=144)
