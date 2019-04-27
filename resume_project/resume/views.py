e2c0109d5bb1b31c356d36630637962f35d777b5
Fedor Chikhachev <kal3vala@MacBook-Pro-Fedor.local>
2019-04-27 18:50:42 +0300

    fixing small bug

--- resume_project/resume/views.py
+++ resume_project/resume/views.py
@@ -1,7 +1,7 @@
 from random import randint
 
 from django.contrib import messages
-from django.contrib.auth import login, logout, update_session_auth_hash,authenticate
+from django.contrib.auth import login, logout, update_session_auth_hash, authenticate
 from django.contrib.auth.decorators import login_required
 
 from django.contrib.auth.models import User
@@ -11,29 +11,33 @@ from resume.API import github
 from resume import models
 from django.core import serializers
 from django.http import JsonResponse
+
+
 @login_required
 def main(request):  # main page
     data = dict()
     return render(request, "main.html", data)
 
+
 def logout_func(request):
     logout(request)
     return redirect('/login/?next=/')
 
+
 @login_required
 def test(request):
     data = dict()
-    if models.GithubConnectedUsers.objects.filter(authorid = request.user.id).exists():
+    if models.GithubConnectedUsers.objects.filter(authorid=request.user.id).exists():
         print('profile exists')
-        tmp = models.GithubConnectedUsers.objects.filter(authorid = request.user.id).values()
-        #tmp = tmp[0]
+        tmp = models.GithubConnectedUsers.objects.filter(authorid=request.user.id).values()
+        # tmp = tmp[0]
 
-        return JsonResponse(list(tmp)[0],safe=False)
+        return JsonResponse(list(tmp)[0], safe=False)
     else:
-        github(request.user.id,request.user.last_name, request.user.username)
-
+        github(request.user.id, request.user.last_name, request.user.username)
 
     return render(request, "main.html", data)
+
 
 @login_required
 def test_pictures(request):
@@ -41,27 +45,25 @@ def test_pictures(request):
     if models.GithubConnectedUsers.objects.filter(authorid=request.user.id).exists():
         print('profile exists')
 
-        data['pics'] = ['/static/users_dir/' + request.user.username + '/demo.png', '/static/users_dir/' + request.user.last_name + '/demo2.png']
+        data['pics'] = ['/static/users_dir/' + request.user.username + '/demo.png',
+                        '/static/users_dir/' + request.user.username + '/demo2.png']
 
         return render(request, "plots.html", data)
 
     else:
-        github(request.user.id,request.user.last_name, request.user.username)
-
+        github(request.user.id, request.user.last_name, request.user.username)
 
     return render(request, "main.html", data)
 
 
 def test_for_swift_app(request):
     data = dict()
-    
+
     login_from_app = request.GET['login']
     pass_from_app = request.GET['pass']
     print(login_from_app)
-    user = User.objects.get(username = login_from_app)
+    user = User.objects.get(username=login_from_app)
     if user.check_password(pass_from_app):
-        return redirect("/test",request)
-
-
+        return redirect("/test", request)
 
     return render(request, "main.html", data)
